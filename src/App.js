0e14a6c10f42e282da2ea0577edda49c18aa7830
mosidrum <mosiokanga@gmail.com>
2023-02-08 15:31:31 +0100

    continuing later, i am going to rest for church

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import RecipeCard from "./components/RecipeCard";
+import SearchBar from "./components/SearchBar";
+
+const apiUrl = "https://www.themealdb.com/api/json/v1/1/search.php?s=";
 
 function App() {
+  const [isLoading, setIsLoading] = useState(false);
+  const [query, setQuery] = useState("");
+  const [recipes, setRecipes] = useState([]);
+
+  //Function to fetch data from the api
+  const searchRecipes = async () => {
+    setIsLoading(true);
+    const url = apiUrl + query;
+    const res = await fetch(url);
+    const data = await res.json();
+    console.log(data);
+    setRecipes(data.meal);
+    setIsLoading(false);
+  };
+
+  useEffect(() => {
+    searchRecipes();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+      <h1>My Recipes </h1>
+      <div className="recipes">
+        { recipes ? recipes.map(recipe =>{
+          <RecipeCard
+          key={recipe.idMeal}
+          recipe={recipe} />
+        }) : 'NO RECIPES!'}
+      </div>
     </div>
   );
 }
